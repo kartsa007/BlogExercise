6f71f95bcdef6657d199841a2fb46675739be10a
Kari Heinonen <khe.muhku@gmail.com>
2018-01-21 22:07:45 +0200

    React-redux version started

---
+++ react-redux/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
+render(
+  <Provider store={store} >
+    <Router >
+      <Route path='/' component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
---
+++ react-redux/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+//var loaders = require('./webpack.loaders');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+//const CleanWebpackPlugin = require('clean-webpack-plugin')
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true
+  },
+  plugins: [
+    //    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Blog exercise',
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'url-loader?limit=10000',
+          'img-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.css']
+  },
+}
