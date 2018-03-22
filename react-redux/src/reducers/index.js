def234ff5cd307898af7de5ad24a2d89a33b2c8f
Kari Heinonen <khe.muhku@gmail.com>
2018-03-22 19:02:00 +0200

    Reducers and server added

---
+++ react-redux/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import navigate from './navigate';
+
+const rootReducer = combineReducers({
+  navigate,
+});
+
+export default rootReducer;
---
+++ react-redux/src/reducers/navigate.js
@@ -0,0 +1,15 @@
+import { SIGN_IN } from '../constants/ActionTypes';
+const initialState = {
+
+};
+
+export default function navigate(state = initialState, action) {
+  switch (action.type) {
+    case SIGN_IN:
+      return {
+
+      };
+    default:
+      return state;
+  }
+}
